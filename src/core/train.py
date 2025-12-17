Reduced RRT waypoints for rollout, try to fix the scaling issue for the actions, added way for extracting RRT waypoints (untested though)

src/core/train.py
@@ -119,7 +119,7 @@ def run_training(config: dict, device: torch.device, use_tqdm: bool = True) -> f
             ep_reward += r
             
             fsm_state = fsm.transition(s_next)
-            done = info['is_collision'] or (fsm_state == FSM_STATE_GOAL)
+            done = info['is_collision'] or (fsm_state == FSMAutomaton.FSM_STATE_GOAL)
             
             buffer.add(s, a, s_next, g, r, done, info['h_star'], v_star)
             s = s_next
@@ -163,8 +163,13 @@ def run_training(config: dict, device: torch.device, use_tqdm: bool = True) -> f
                     batch = buffer.sample(train_config['batch_size'])
                     b_s = torch.from_numpy(batch['states']).float().to(device)
                     b_g = torch.from_numpy(batch['subgoals']).float().to(device)
-                    b_a = policy_net(b_s, b_g)
-                    b_s_next_pred = dynamics_net(b_s, b_a)
+                    # Policy outputs actions in [-1, 1] range (tanh)
+                    b_a_unscaled = policy_net(b_s, b_g)
+                    # Scale actions to actual range (same as stored in buffer)
+                    b_a_scaled = b_a_unscaled.clone()
+                    b_a_scaled[:, 0] = b_a_unscaled[:, 0] * env.a_max
+                    b_a_scaled[:, 1] = b_a_unscaled[:, 1] * env.omega_max
+                    b_s_next_pred = dynamics_net(b_s, b_a_scaled)
                     policy_optim.zero_grad()
                     for param in cbf_net.parameters(): param.requires_grad = False
                     for param in clf_net.parameters(): param.requires_grad = False
@@ -181,7 +186,7 @@ def run_training(config: dict, device: torch.device, use_tqdm: bool = True) -> f
         if use_tqdm:
             logger.log_scalar("episode/reward", ep_reward, episode)
             logger.log_scalar("episode/length", t+1, episode)
-            logger.log_scalar("episode/success", 1.0 if fsm_state == FSM_STATE_GOAL else 0.0, episode)
+            logger.log_scalar("episode/success", 1.0 if fsm_state == FSMAutomaton.FSM_STATE_GOAL else 0.0, episode)
         
         if ep_reward > best_ep_reward:
             best_ep_reward = ep_reward

src/planning/fsm_planner.py
@@ -26,6 +26,10 @@ class FSMAutomaton:
         self.fsm_config = config['fsm']
         self.clf_config = config['train']
         
+        # Store action limits for scaling (needed for dynamics model)
+        self.a_max = config['env']['a_max']
+        self.omega_max = config['env']['omega_max']
+        
         # Get waypoints from config (list of [x, y] positions)
         waypoint_positions = config["fsm"].get("waypoints", [])
         waypoint_positions = [np.array(wp) for wp in waypoint_positions]
@@ -154,8 +158,14 @@ class FSMAutomaton:
             g_transition = g_transition.repeat(s.shape[0], 1)
             
             with torch.no_grad():
-                a = policy_net(s, g_transition)
-                s_prime = dynamics_net(s, a)
+                # Policy outputs actions in [-1, 1] range (tanh)
+                a_unscaled = policy_net(s, g_transition)
+                # Scale actions to actual range before passing to dynamics model
+                # The dynamics model was trained on scaled actions
+                a_scaled = a_unscaled.clone()
+                a_scaled[:, 0] = a_unscaled[:, 0] * self.a_max  # Scale linear acceleration
+                a_scaled[:, 1] = a_unscaled[:, 1] * self.omega_max  # Scale angular velocity
+                s_prime = dynamics_net(s, a_scaled)
                 
                 h_prime = cbf_net(s_prime)
                 safe_margin = self.fsm_config['safe_margin']